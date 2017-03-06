83ac8a559e4acf28d01f0c01c6e03aeb5d642968
intfox <velkom.v.mi.iashik@inbox.ru>
2017-03-06 10:44:30 +0700

    update .gitignore

--- main.cpp
+++ main.cpp
@@ -6,19 +6,21 @@
 using namespace std;
 
 
+
 int main() {
     srand(time(NULL));
     SDL_Init(SDL_INIT_EVERYTHING);
-    int size_windows_w = 1280, size_windows_h = 720;
-    SDL_Window *win = SDL_CreateWindow("Myprj", 0, 0, size_windows_w, size_windows_h);
-    SDL_Render *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    int size_windows_w = 1200, size_windows_h = 800;
+    SDL_Window *win = SDL_CreateWindow("Myprj", 0, 0, size_windows_w, size_windows_h, SDL_WINDOW_OPENGL);
+    SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
     SDL_Event event;
     bool quit = true;
     while(quit) {
-        if (event.type == SDL_QUIT) quit = false;
         SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-        SDL_RenderClear(ren);
+	    SDL_RenderClear(ren);
+	    SDL_PollEvent(&event);
+	    if (event.type == SDL_QUIT) quit = false;
         SDL_RenderPresent(ren);
     }
-    cout << "helloworld motherfucker";
+	SDL_Quit;
 }
